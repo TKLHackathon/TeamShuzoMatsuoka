2bf1ec5ea144de984b1826a5637756a0cb84b9be
Yudai0694 <you13bskwk197@gmail.com>
2017-09-06 13:43:50 +0900

    Yudai

---
+++ index.php
@@ -0,0 +1,20 @@
+﻿<?php
+echo "kurashita";
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf8">
+    <title>はじめてのHTML</title>
+  </head>
+  <body>
+    <p id="message"><a href="http://dotinstall.com">こんばんわ！
+    <form action="reference.html" method="post">
+      <p><input type="submit" value="Start!!!" name="sessionID"></p>
+
+      <button type="button" name="sessionID2">Start!!</button>
+    </form>
+
+  </body>
+</html>
